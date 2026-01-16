6d688cc388483be526836c302031f9663a96def6
ahyan006 <ahyan_amin@outlook.com>
2026-01-16 14:41:38 -0700

    done

--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 ## Student Details
 
-- **Full Name:** `<Enter name>`
-- **CCID:** `<Enter ccid>`
+- **Full Name:** `Ahyan Amin`
+- **CCID:** `ahyan2`
 
 ## References and Resources
 
 List any resources used here, or simply put `N/A` if not applicable.
+N/A
 
 ## Verbal Collaboration
 
--- app/src/main/java/com/example/listcity/MainActivity.java
+++ app/src/main/java/com/example/listcity/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.listcity;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -18,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+    Button addBtn;
+    Button deleteBtn;
+    int selectedPos = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +37,59 @@ public class MainActivity extends AppCompatActivity {
         });
 
         cityList = findViewById(R.id.city_list);
+        addBtn = findViewById(R.id.btn_add_city);
+        deleteBtn = findViewById(R.id.btn_delete_city);
 
-        String []cities = {"Edmonton", "Vancouver", "Moscow", "Sydney", "Berlin", "Vienna", "Tokyo", "Beijing", "Osaka", "New Dheli"};
+        String[] cities = {
+                "Edmonton", "Vancouver", "Moscow", "Sydney",
+                "Berlin", "Vienna", "Tokyo", "Beijing",
+                "Osaka", "New Dheli"
+        };
 
         dataList = new ArrayList<>();
         dataList.addAll(Arrays.asList(cities));
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        // Selectnig a city
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPos = position;
+            }
+        });
+
+        // add city
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = new EditText(MainActivity.this);
+                input.setHint("City name");
+
+                new AlertDialog.Builder(MainActivity.this)
+                        .setTitle("Add City")
+                        .setView(input)
+                        .setPositiveButton("CONFIRM", (dialog, which) -> {
+                            String city = input.getText().toString();
+                            dataList.add(city);
+                            cityAdapter.notifyDataSetChanged();
+                        })
+                        .setNegativeButton("CANCEL", null)
+                        .show();
+            }
+        });
+
+        // delete city that was selected
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPos != -1) {
+                    dataList.remove(selectedPos);
+                    selectedPos = -1;
+                    cityAdapter.notifyDataSetChanged();
+                }
+            }
+        });
     }
 }
